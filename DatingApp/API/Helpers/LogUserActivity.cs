cb988946458499b904a5ff6b0fbe674aa17fae5e
Connor Nethen <ConnorNethen258@gmail.com>
2025-09-23 13:30:01 -0500

    End of section 18

--- DatingApp/API/Helpers/LogUserActivity.cs
+++ DatingApp/API/Helpers/LogUserActivity.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace API.Helpers;
@@ -13,10 +14,10 @@ public class LogUserActivity : IAsyncActionFilter
 
         var userId = resultContext.HttpContext.User.GetUserId();
 
-        var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
-        var user = await repo.GetUserByIdAsnyc(userId);
+        var unitOfWork = resultContext.HttpContext.RequestServices.GetRequiredService<IUnitOfWork>();
+        var user = await unitOfWork.UserRepository.GetUserByIdAsnyc(userId);
         if (user == null) return;
         user.LastActive = DateTime.UtcNow;
-        await repo.SaveAllAsync();
+        await unitOfWork.Complete();
     }
 }
